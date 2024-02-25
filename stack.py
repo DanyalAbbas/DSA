027608b7939fb06918ab19c1dec4319adf676182
Danyal Abbas <abasdaniel61@gmail.com>
2024-02-25 12:13:27 +0500

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+# stack = deque()
+
+# print(dir(stack))
+
+class Stack():
+    def __init__(self):
+        self.jar = deque()
+    
+    def push(self,val):
+        self.jar.append(val)
+
+    def pop(self):
+        return self.jar.pop()
+    
+    def peek(self):
+        return self.jar[-1]
+    
+    def is_empty(self):
+        return len(self.jar == 0)
+    
+    def size(self):
+        return len(self.jar)
+
+    def reverse_string(self):
+        if type(self.peek()) == str:
+            return self.peek()[::-1]
+        else:
+            raise Exception("Invalid Input")
+
+
+stack = Stack()
+stack.push("hello")
+stack.push(2)
+stack.push("We will conquere COVID-19")
+
+
+print(stack.reverse_string())
+
+
+    
+
