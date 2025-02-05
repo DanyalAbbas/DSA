adced4c9bc1698c9442addb5675a46f367caf017
Danyal Abbas <abasdaniel61@gmail.com>
2025-02-05 23:58:34 +0500

    Hash table exercise 1 and 2

---
+++ Exercises/Hash_Table_E.py
@@ -0,0 +1,22 @@
+with open("nyc_weather.csv", "r") as rf:
+    arr = []
+    for i in rf.readlines()[1::]:
+        day, temp = i.split(",")
+        arr.append(int(temp))
+
+print(sum(arr[:7])/7)
+print(max(arr[:10]))
+
+
+weather_dict = {}
+
+with open("nyc_weather.csv","r") as rf:
+    for i in rf.readlines()[1::]:
+        day, temp = i.split(",")
+        temp = int(temp)
+        weather_dict[day] = temp
+    
+print(weather_dict)
+
+print(weather_dict["Jan 9"])
+print(weather_dict["Jan 4"])
