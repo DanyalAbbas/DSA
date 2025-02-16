7e69fa4ae776154e8351376a9acaac96f8c35048
Danyal Abbas <abasdaniel61@gmail.com>
2025-02-16 17:44:59 +0500

    Update QuickSort_E.py

--- Exercises/QuickSort_E.py
+++ Exercises/QuickSort_E.py
@@ -4,27 +4,23 @@ def swap(a,b, arr):
 
 def partition(elements, start, end):
     pivot = elements[end]
-
     P_index = start
-    while P_index < end:
-        while elements[P_index] < pivot:
-            P_index += 1
-        i = P_index
-        while i <= end and elements[i] > pivot:
-            i += 1
 
-        if P_index < i: swap(P_index, i, elements)
-        if i == end: break    
-        
+    for i in range(start, end):
+        if elements[i] < pivot:  
+            swap(i, P_index, elements)
+            P_index += 1
+
+    swap(P_index, end, elements)
     return P_index
 
 def QuickSort(elements, start, end):
     if start < end:
         pi = partition(elements, start, end)
-        QuickSort(elements, pi+1, end)
         QuickSort(elements, start, pi-1)
+        QuickSort(elements, pi+1, end)
 
 if __name__ == "__main__":
-    elements = [11,9,29,7,2,28,15]
+    elements = [29,11,9,7,2,28,15]
     QuickSort(elements, 0, len(elements) - 1)
     print(elements)
