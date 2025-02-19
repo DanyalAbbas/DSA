adee6a6d04e3fefaac48e750fcb34af19c3975e1
Danyal Abbas <abasdaniel61@gmail.com>
2025-02-19 19:34:53 +0500

    added DS pdf insertion sort code to try out on python debugger

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,9 +5,20 @@ def InsertionSort(elements):
         while j >= 0 and anchor < elements[j]:
             elements[j+1] = elements[j]
             j = j - 1
-        elements[j+1] = anchor  
+        elements[j+1] = anchor
+
+def InsertionSortinDS(elements):
+     for j in range(1,len(elements)):
+        i = 0
+        while elements[j] > elements[i]:
+            i += 1
+        m = elements[j]
+        for k in range(j-i):
+            elements[j-k] = elements[j-k-1]
+        elements[i] = m
 
 if __name__ == "__main__":
-    elements = [11,9,29,7,2,15,28]
-    InsertionSort(elements)
+    elements = [9,11,29,7,2,15,28]
+    # InsertionSort(elements)
+    InsertionSortinDS(elements)
     print(elements)
